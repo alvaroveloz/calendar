59482f29e3b599f1d339198a28bee8ca6b51eeec
alvaroveloz <alvaro.veloz@gmail.com>
2023-02-08 22:04:18 -0500

    CALENDAR - Added Redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { AppRouter } from './router/AppRouter';
-import { BrowserRouter } from 'react-router-dom';
 import './styles.css';
+import { CalendarApp } from './CalendarApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+  <React.StrictMode>    
+      <CalendarApp />    
   </React.StrictMode>
 );
