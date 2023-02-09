7c00673567f6d14d0cb4b2e4c39b76f17ddefe77
alvaroveloz <alvaro.veloz@gmail.com>
2023-02-08 21:46:28 -0500

    CALENDAR - Added simple form validation

--- README.md
+++ README.md
@@ -3,5 +3,9 @@ Calendar App using React with Vite
 
 ## Modules
 ### react-modal
+yarn add react-modal
+### sweetalert2
+
+yarn add sweetalert2
 
 
--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
-import ReactModal from 'react-modal';
+import { useMemo, useState } from 'react';
+import { addHours, differenceInSeconds } from 'date-fns';
+import Swal from 'sweetalert2';
 
+import ReactModal from 'react-modal';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { addHours } from 'date-fns';
 
 const customStyles = {
   content: {
@@ -27,7 +28,15 @@ const formData = {
 
 export const CalendarModal = () => {
   const [isOpen, setIsOpen] = useState(true);
+  const [formSubmitted, setFormSubmitted] = useState(false);
   const [formValues, setFormValues] = useState(formData);
+
+  const isInvalid = useMemo(() => { 
+  if (!formSubmitted) return '';
+  return formValues.title.length >0 
+  ? ''
+  : 'is-invalid';
+}, [formValues.title, formSubmitted])
 
   const onCloseModal = () => {
     console.log('Closing');
@@ -43,6 +52,25 @@ export const CalendarModal = () => {
     setFormValues( ( current ) => ( {...current, [datePickerSelected]: event} ))
   };
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+    const difference = differenceInSeconds(formValues.end, formValues.start);
+    
+    if( isNaN(difference) ) {
+      Swal.fire('Incorrect dates', 'Start and end dates should be filled.', 'error');
+      return ;
+    }
+
+    if (difference <= 0) {
+      Swal.fire('Incorrect dates', 'End date should be greater that start date', 'error');
+      return;
+    }
+    
+    console.log(formValues);
+    
+  }
+
   return (
     <ReactModal
       isOpen={isOpen}
@@ -52,9 +80,9 @@ export const CalendarModal = () => {
       overlayClassName='modal-fondo'
       closeTimeoutMS={200}
     >
-      <h1> Nuevo evento </h1>
+      <h1> New Event </h1>
       <hr />
-      <form className='container'>
+      <form className='container' onSubmit={onSubmit}>
         <div className='form-group mb-2'>
           <label>Fecha y hora inicio</label>
           {/* <input className='form-control' placeholder='Fecha inicio' /> */}
@@ -63,17 +91,19 @@ export const CalendarModal = () => {
             className='form-control'
             onChange={(date) => onDateChange(date, 'start')}
             dateFormat='Pp'
+            showTimeSelect
           />
         </div>
 
         <div className='form-group mb-2'>
           <label>Fecha y hora fin</label>
           <DatePicker
-          minDate={formValues.start}
+            minDate={formValues.start}
             selected={formValues.end}
             className='form-control'
             onChange={(date) => onDateChange(date, 'end')}
             dateFormat='Pp'
+            showTimeSelect
           />
         </div>
 
@@ -82,7 +112,7 @@ export const CalendarModal = () => {
           <label>Titulo y notas</label>
           <input
             type='text'
-            className='form-control'
+            className={`form-control ${isInvalid}`}
             placeholder='Título del evento'
             name='title'
             autoComplete='off'
