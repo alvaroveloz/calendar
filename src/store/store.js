c7c76281516d1d34a5c11757ee8faa6cf3080dad
alvaroveloz <alvaro.veloz@gmail.com>
2023-02-11 13:34:38 -0500

    CALENDAR - Fixed error serializableCheck with Dates

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,10 @@ import { calendarSlice, uiSlice } from './';
 
 
 export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
   reducer: {
     calendar: calendarSlice.reducer,
     ui: uiSlice.reducer,
