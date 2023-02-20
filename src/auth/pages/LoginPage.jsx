24dda81ee003b33382a2e2cddf8ac620d2b5b4c3
alvaroveloz <alvaro.veloz@gmail.com>
2023-02-20 12:55:27 -0500

    CALENDAR - using auth hook

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,18 +1,77 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useForm } from '../../hooks';
+import { useAuthStore } from '../../hooks/useAuthStore';
+
+import Swal from 'sweetalert2';
 import './LoginCss.css';
 
+const loginFormFields = {
+  loginEmail: '',
+  loginPassword: '',
+};
+
+const registerFormFields = {
+  registerName: '',
+  registerEmail: '',
+  registerPassword: '',
+  registerPasswordRepeat: '',
+};
+
 export const LoginPage = () => {
+
+  const { startLogin, errorMessage } = useAuthStore();
+
+  const {
+    loginEmail,
+    loginPassword,
+    onInputChange: onLoginInputChange,
+  } = useForm(loginFormFields);
+  const {
+    registerName,
+    registerEmail,
+    registerPassword,
+    registerPasswordRepeat,
+    onInputChange: onRegisterInputChange,
+  } = useForm(registerFormFields);
+
+  const loginSubmit = (event) => {
+    event.preventDefault();
+    startLogin({ email: loginEmail, password: loginPassword });
+  };
+
+  const registerSubmit = (event) => {
+    event.preventDefault();
+    console.log({
+      registerName,
+      registerEmail,
+      registerPassword,
+      registerPasswordRepeat,
+    });
+  };
+
+
+  useEffect(() => {
+  if (errorMessage !== undefined ) {
+    Swal.fire('Authentication error', errorMessage, 'error');
+  }  
+      
+  }, [ errorMessage ])
+  
+
   return (
     <div className='container login-container'>
       <div className='row'>
         <div className='col-md-6 login-form-1'>
           <h3>Ingreso</h3>
-          <form>
+          <form onSubmit={loginSubmit}>
             <div className='form-group mb-2'>
               <input
                 type='text'
                 className='form-control'
                 placeholder='Correo'
+                name='loginEmail'
+                value={loginEmail}
+                onChange={onLoginInputChange}
               />
             </div>
             <div className='form-group mb-2'>
@@ -20,6 +79,9 @@ export const LoginPage = () => {
                 type='password'
                 className='form-control'
                 placeholder='Contraseña'
+                name='loginPassword'
+                value={loginPassword}
+                onChange={onLoginInputChange}
               />
             </div>
             <div className='form-group mb-2'>
@@ -30,12 +92,15 @@ export const LoginPage = () => {
 
         <div className='col-md-6 login-form-2'>
           <h3>Registro</h3>
-          <form>
+          <form onSubmit={registerSubmit}>
             <div className='form-group mb-2'>
               <input
                 type='text'
                 className='form-control'
                 placeholder='Nombre'
+                name='registerName'
+                value={registerName}
+                onChange={onRegisterInputChange}
               />
             </div>
             <div className='form-group mb-2'>
@@ -43,6 +108,9 @@ export const LoginPage = () => {
                 type='email'
                 className='form-control'
                 placeholder='Correo'
+                name='registerEmail'
+                value={registerEmail}
+                onChange={onRegisterInputChange}
               />
             </div>
             <div className='form-group mb-2'>
@@ -50,6 +118,9 @@ export const LoginPage = () => {
                 type='password'
                 className='form-control'
                 placeholder='Contraseña'
+                name='registerPassword'
+                value={registerPassword}
+                onChange={onRegisterInputChange}
               />
             </div>
 
@@ -58,6 +129,9 @@ export const LoginPage = () => {
                 type='password'
                 className='form-control'
                 placeholder='Repita la contraseña'
+                name='registerPasswordRepeat'
+                value={registerPasswordRepeat}
+                onChange={onRegisterInputChange}
               />
             </div>
 
